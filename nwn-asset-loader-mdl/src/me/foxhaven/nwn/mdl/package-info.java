9afcf7081746320664152b8a59ee455ee7ef83f4
Charles Hayward <rolo@amethysttapestry.com>
2014-09-01 13:03:40 -0700

    changed mimetype from text/mdl to text/x-mdl

---
+++ nwn-asset-loader-mdl/src/me/foxhaven/nwn/mdl/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "MDLTemplate.mdl")
+package me.foxhaven.nwn.mdl;
+
+import org.netbeans.api.templates.TemplateRegistration;
